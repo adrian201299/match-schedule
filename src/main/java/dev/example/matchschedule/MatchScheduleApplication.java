a0e366abd6378a45101725ccba00b4c1f113682f
adrian201299 <74474514+adrian201299@users.noreply.github.com>
2022-09-30 02:12:27 +0200

    Add files via upload

---
+++ src/main/java/dev/example/matchschedule/MatchScheduleApplication.java
@@ -0,0 +1,13 @@
+package dev.example.matchschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchScheduleApplication.class, args);
+	}
+
+}
